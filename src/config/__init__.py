8ec544d07eaec0433ec7b30ec4e5590bba9ddb3e
Arthur Acker <arthuracker07@gmail.com>
2025-10-05 13:17:11 -0500

    changes

--- src/config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Configuration helpers for Impactor-2025 MVP."""
-
-from .slider_specs import SLIDER_SPECS, SliderSpec, get_slider_spec
-
-__all__ = [
-    "SLIDER_SPECS",
-    "SliderSpec",
-    "get_slider_spec",
-]
